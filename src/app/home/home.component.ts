a2fb56e82d87f543929bb279138bd46e18018ff1
soshiant1 <42187758+soshiant1@users.noreply.github.com>
2018-08-09 21:57:00 +0430

    Final Comment by Soashiant

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { IComment } from './../icomment';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  comments: IComment[] = [];
+  @ViewChild('txtComment') txtComment: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+  }
+  addComment(){
+
+    let c: IComment= {
+      comment: this.txtComment.nativeElement.value,
+      like: 0,
+      dislike: 0,
+      numberOfReviews: 0,
+      star: 0
+    }
+
+    this.comments.push(c);
+
+  }
+
+  reviewsCount(c: IComment, n: HTMLInputElement){
+    c.numberOfReviews++;
+    c.star += parseInt(n.value);
+  }
+  commentLike(c: IComment){
+    c.like++;
+  }
+  commentDisLike(c: IComment){
+    c.dislike++;
   }
 
 }
--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.css']
 })
-export class StarComponent implements OnInit {
+export class StarComponent implements OnInit, OnChanges {
+
+  @Input() rating: number;
+  width: number;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngOnChanges(){
+    this.width = this.rating * 86 / 5;
+  }
+
 
 }
